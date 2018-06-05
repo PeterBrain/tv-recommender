11ff26f5e65ef74aecc543c9f598162824f862d1
Peter <peter.loecker@live.at>
2018-06-06 00:54:48 +0200

    Feature Set 1 & 3 - build commit #5 & #1
    Feature Set 1:
    very little changes
    
    Feature Set 3 (Actors):
    Implemented everything except sorting

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,9 @@ module Main where
 
 --import Lib
 --import Data.Char
+import System.IO
+import System.IO.Error
+import System.Directory
 import Text.HTML.TagSoup
 import Network.HTTP.Conduit
 import qualified Data.ByteString.Lazy.Char8 as L8
@@ -15,29 +18,58 @@ data TvProgramEntry = TvProgramEntry {
   genre :: String, -- its genre (if available)
   link :: String} deriving (Read, Eq, Show)
 
+
 --instance Show TvProgramEntry where
 --  show program = number program ++ ". " ++ time program ++ " " ++ station program ++ "\t" ++ title program ++ ", " ++ genre program ++ "\n"
 
 printTvProgram :: TvProgramEntry -> IO ()
 printTvProgram program = putStrLn $ number program ++ ". " ++ time program ++ " " ++ station program ++ "\t" ++ title program ++ ", " ++ genre program
 
-type TvProgram = [TvProgramEntry]
 
-main :: IO ()
+--main :: IO ()
 main = do
-  getTvProgram
+  putStrLn "Loading Programm ..."
   loop
 
 
-getTvProgram :: IO ()
+loop :: IO ()
+loop = do
+  putStrLn "\nPlease enter a command or type 'help' for assistance!"
+  command <- getLine
+  let hcommand = words command
+  --if length hcommand == 1 then print command else print $ head hcommand
+  case command of
+    "list" -> listTvProgram
+    --"show" -> show
+    --"add actor" -> actorAdd
+    "list actors" -> actorList
+    --"delete actor" -> actorDel
+    --"recommend" -> recommend
+    "exit" -> putStrLn "Bye!"
+    "help" -> help
+    _ -> do
+      putStrLn $ "Command '" ++ command ++ "' is unknown!"
+      loop
+
+
+help = do
+  putStrLn "\nThis Programm supports the following commands:"
+  putStrLn "\t'list' ... shows an overview of all broadcasts"
+  --putStrLn "\t'show' n ... shows the details of the n-th entry in the programm list"
+  putStrLn "\t'add actor' name ... adds the given name to your list of favorite actors"
+  putStrLn "\t'list actors' ... shows a list of all your favorite actors"
+  putStrLn "\t'delete actor' name ... removes the given name from your list of favorite actors"
+  --putStrLn "\t'recommend' ... shows a list of broadcast featuring your favorite actors"
+  putStrLn "\t'exit' ... terminate the application"
+  putStrLn "\t'help' ... shows this message"
+  loop
+
+
+--getTvProgram :: IO ()
 getTvProgram = do
   putStrLn "Loading Programm ..."
   content <- parseTags <$> simpleHttp "https://www.tele.at/tv-programm/2015-im-tv.html?stationType=-1&start=0&limit=5&format=raw"
   --print content -- prints complete parsed content
-
-  --L8.putStrLn $ innerText $ dropWhile (~/= "<div class=station>") content
-  --L8.putStrLn $ fromTagText $ dropWhile (~/= "<div class=station>") content !! 1
-  --print $ head $ L8.words $ innerText $ dropWhile (~/= "<div class=genre>") content
 
   --print $ L8.filter (/='\t') $ L8.filter (/='\n') $ fromTagText $ head $ tail $ dropWhile (~/= TagOpen "" [("class","genre")]) content -- print all (everything)
 
@@ -52,60 +84,93 @@ getTvProgram = do
   --let parsedHtml = fmap (\x -> L8.filter (/='\t') $ L8.filter (/='\n') $ fromTagText x) $ filter isTagText $ takeWhile (~/= TagOpen "" [("class","watchlist add")]) $ dropWhile (~/= TagOpen "" [("class","bc-content-container")]) content
   --print $ filter (/="") $ map (L8.unpack) parsedHtml
 
-  --let listofprogramms = filter (/="") $ map L8.unpack parsedHtml
-  --let myentry = TvProgramEntry "000" (head $ tail listofprogramms) (head $ tail $ tail listofprogramms) (head $ tail $ tail $ tail listofprogramms) (head $ tail $ tail $ tail $ tail listofprogramms) ""
-  --print myentry
-
-  let splittedHtml = map (map (\x -> L8.filter (/='\t') $ L8.filter (/='\n') $ fromTagText x)) $ map (\x -> filter isTagText $ takeWhile (~/= TagOpen "" [("class","watchlist add")]) $ dropWhile (~/= TagOpen "" [("class","bc-content-container")]) x) $ sections (~== "<div class=bc-item>") content
+  let splittedHtml = map (map (\x -> L8.filter (/='\t') $ L8.filter (/='\n') $ fromTagText x)) $ map (\x -> filter isTagText $ filter (~/= TagOpen "" [("class","episode")]) $ takeWhile (~/= TagOpen "" [("class","watchlist add")]) $ dropWhile (~/= TagOpen "" [("class","bc-content-container")]) x) $ sections (~== "<div class=bc-item>") content
 
   let listofprogramms2 = map (\x -> filter (/="") $ map L8.unpack x) splittedHtml
 
-  --let mynewentry = map (\x -> TvProgramEntry "000" (head $ tail x) (head $ tail $ tail x) (head $ tail $ tail $ tail x) (head $ tail $ tail $ tail $ tail x) "") listofprogramms2
-
-  let mynewentry = map (\x -> TvProgramEntry "000" (x !! 1) (x !! 2) (x !! 3) (x !! 4) "") listofprogramms2
-  mapM_ printTvProgram mynewentry
-  --print mynewentry
+  let entries = map (\x -> if length x == 6 then TvProgramEntry "000" (x !! 1) (x !! 2) (x !! 3) (x !! 5) "" else TvProgramEntry "000" (x !! 1) (x !! 2) (x !! 3) (x !! 4) "") listofprogramms2
+  --print entries
+  --listTvProgram entries
+  --mapM_ printTvProgram entries
+  return entries
 
   putStrLn "Got Content! Reading broadcast details ..."
 
 
 listTvProgram :: IO ()
 listTvProgram = do
-  --let manually = [TvProgramEntry {number="001", time= "20:15-21:45", station= "ORF eins", title= "DOKeins: Bauer unser", genre= "Dokumentarfilm, \195\150/BEL/F 2016", link= ""}, TvProgramEntry {number="002", time= "20:15-21:00", station= "ZDF infokanal", title= "Madame Mao - Aufstieg und Fall der Jiang Qing", genre= "Dokumentation, D 2017", link= ""}, TvProgramEntry {number="003", time= "20:15-21:45", station= "ORF eins", title= "DOKeins: Bauer unser", genre= "Dokumentarfilm, \195\150/BEL/F 2016", link= ""}, TvProgramEntry {number="004", time= "20:15-21:00", station= "ZDF infokanal", title= "Madame Mao - Aufstieg und Fall der Jiang Qing", genre= "Dokumentation, D 2017", link= ""}]
-  --let manually2 = TvProgramEntry "001" "20:15-21:45" "ORF eins" "DOKeins: Bauer unser" "Dokumentarfilm, \195\150/BEL/F 2016" ""
-  --print manually
-  --print manually2
-
+  --mapM_ printTvProgram =<< getTvProgram
+  print =<< getTvProgram
   loop
 
 
-loop :: IO ()
-loop = do
-  putStrLn "\nPlease enter a command or type 'help' for assistance!"
-  command <- getLine
-  case command of
-    "list" -> listTvProgram
-    --"show" -> show
-    --"add actor" -> actorAdd
-    --"list actors" -> actorsList
-    --"delete actor" -> actorDel
-    --"recommend" -> recommend
-    "exit" -> putStrLn "Bye!"
-    "help" -> help
-    _ -> do
-      putStrLn $ "Command '" ++ command ++ "' is unknown!"
-      loop
+find' :: (a->Bool) -> [a] -> Bool
+find' _ [] = False
+find' func (h:t)
+  | func h = True
+  | otherwise = find' func t
 
 
-help :: IO ()
-help = do
-  putStrLn "\nThis Programm supports the following commands:"
-  putStrLn "\t'list' ... shows an overview of all broadcasts"
-  --putStrLn "\t'show' n ... shows the details of the n-th entry in the programm list"
-  --putStrLn "\t'add actor' name ... adds the given name to your list of favorite actors"
-  --putStrLn "\t'list actors' ... shows a list of all your favorite actors"
-  --putStrLn "\t'delete actor' name ... removes the given name from your list of favorite actors"
-  --putStrLn "\t'recommend' ... shows a list of broadcast featuring your favorite actors"
-  putStrLn "\t'exit' ... terminate the application"
-  putStrLn "\t'help' ... shows this message"
+actorAdd :: String -> IO ()
+actorAdd name = do
+  let
+      outFile = "actors.txt"
+      tempFile = "actors.tmp"
+
+  fileExist <- doesFileExist outFile
+  if not fileExist then writeFile outFile "" else return ()
+
+  handle <- openFile outFile ReadMode
+  fileContent <- hGetContents handle
+
+  let
+    append
+      | not checkDouble = fileContent ++ name ++ "\n"
+      | otherwise = fileContent
+    checkDouble = find' (==name) (lines fileContent)
+
+  writeFile tempFile append
+
+  hClose handle
+  removeFile outFile
+  renameFile tempFile outFile
   loop
+
+
+actorDel :: String -> IO ()
+actorDel name = do
+  let
+      outFile = "actors.txt"
+      tempFile = "actors.tmp"
+
+  handle <- openFile outFile ReadMode
+  fileContent <- hGetContents handle
+
+  let filterFile = unlines $ filter (/=name) (lines fileContent)
+  writeFile tempFile filterFile
+
+  hClose handle
+  removeFile outFile
+  renameFile tempFile outFile
+  loop
+
+  `catchIOError` (\_ -> do
+    putStrLn "You do not have any favorite actors yet. Please use 'add actor' to add one."
+    loop
+  )
+
+
+actorList :: IO ()
+actorList = do
+  let outFile = "actors.txt"
+
+  handle <- openFile outFile ReadMode
+  fileContent <- hGetContents handle
+  putStrLn $ "\n" ++ fileContent
+  hClose handle
+  loop
+
+  `catchIOError` (\_ -> do
+    putStrLn "You do not have any favorite actors yet. Please use 'add actor' to add one."
+    loop
+  )
